595596fa3abd207d06c53e5405cf9b16a0ea33cf
christabel mclain <camclain@princeton.edu>
2019-10-01 19:34:21 -0400

    Question a)

---
+++ Ex 3.jl
@@ -0,0 +1,20 @@
+# Question A
+
+# part a
+
+using JLD
+using PyPlot
+
+image = imread("el-capitan.png")
+imshow(image)
+size(image)
+
+# part b
+
+function extractRGB(String)
+    image = imread(String)
+    imshow(image)
+    R = image[:,:,1]
+    G = image[:,1,:]
+    B = image[1,:,:]
+end
