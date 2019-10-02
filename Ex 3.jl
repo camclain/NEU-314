8e258625a3d2d09769c893c33a28d3da6447d86e
christabel mclain <camclain@princeton.edu>
2019-10-01 20:07:10 -0400

    Question e)

--- Ex 3.jl
+++ Ex 3.jl
@@ -15,8 +15,25 @@ function extractRGB(String)
     image = imread(String)
     imshow(image)
     R = image[:,:,1]
-    G = image[:,1,:]
-    B = image[1,:,:]
-
-    println("Reset with git reset --mixed")
+    G = image[:,:,2]
+    B = image[:,:,3]
+    return [R, G, B]
 end
+
+println("Reset with git reset --mixed")
+
+## part e
+
+image2 = zeros(360, 640, 3)
+image2[:,:,1] = extractRGB("el-capitan.png")[2]
+image2[:,:,2] = extractRGB("el-capitan.png")[3]
+image2[:,:,3] = extractRGB("el-capitan.png")[1]
+
+subplot(1,2,1)
+imshow(image)
+title("Original image")
+axis("off")
+subplot(1,2,2)
+imshow(image2)
+title("Image with inverted RGB values")
+axis("off")
